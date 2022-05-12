58be4d741355625119c6f9e9b1dbba973ac30c0d
GReaperDot <greaper.dot@gmail.com>
2022-05-12 19:53:25 +0300

    none

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        //Parser parser = new Parser();
-        //parser.Start();
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
-        String separateVar1 = "=";
-        String separateVar2 = " ";
+public class Main {
+    private static String separateVar1;
+    private static String separateVar2;
+    private static String charMenu;
+    private static String found;
+    private static String searchFileLine;
+    private static String compensator;
+    private static String filePath = "Emoji.txt";
+    private static int count;
+    private static int linesCount;
+    private static int randomEmoji;
+
+    public static void main(String[] args) throws Exception {
+        separateVar2 = " ";
+        separateVar1 = "=";
+
+        Parser parser = new Parser(); parser.Start();
+        Scanner scanner = new Scanner(System.in);
+
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        Random random = new Random();
+
         System.out.println("+" + separateVar1.repeat(50) + "+\n" +
                 "=" + separateVar2.repeat(9) + "░░░░░██╗░█████╗░██╗░░░██╗░█████╗░" + separateVar2.repeat(8) + "=\n" +
                 "=" + separateVar2.repeat(9) + "░░░░░██║██╔══██╗██║░░░██║██╔══██╗" + separateVar2.repeat(8) + "=\n" +
@@ -15,11 +38,45 @@ public class Main {
                 "+" + separateVar1.repeat(50) + "+\n" +
                 "=" + " Practical Work : Java Emoji : Bielietskiy Radion " + "=\n" +
                 "+" + separateVar1.repeat(50) + "+\n");
-        System.out.println("Available actions:\n" +
-                separateVar2.repeat(17) + "1." + separateVar2.repeat(3) + "Codename Emoji\n" +
-                separateVar2.repeat(17) + "2." + separateVar2.repeat(3) + "Randome Emoji\n" +
-                separateVar2.repeat(17) + "0." + separateVar2.repeat(3) + "Exit.\n");
-        System.out.print("Enter you choise:");
 
+        System.out.println("Available actions:\n" +
+                separateVar2.repeat(17) + "a." + separateVar2.repeat(3) + "Codename Emoji\n" +
+                separateVar2.repeat(17) + "b." + separateVar2.repeat(3) + "Randome Emoji\n" +
+                separateVar2.repeat(17) + "q." + separateVar2.repeat(3) + "Exit.\n");
+
+        do {
+            System.out.print("Enter you choices: ");
+            charMenu = scanner.nextLine();
+            switch (charMenu) {
+                case "a":
+                    System.out.print("Enter Codename Emoji for search: ");
+
+                    found = scanner.nextLine();
+                    searchFileLine = bufferedReader.readLine();
+
+                    while (!searchFileLine.equals(found)){
+                        searchFileLine = bufferedReader.readLine();
+                        count++;
+                    } System.out.println(bufferedReader.readLine());
+                    break;
+
+                case "b":
+                    System.out.print("Randome Emoji: ");
+
+                    Stream<String> linesC = Files.lines(Paths.get(filePath));
+                    linesCount = (int) linesC.count();
+
+                    randomEmoji = random.nextInt(linesCount);
+
+                    for(int i = 1; i <= randomEmoji; i = i + 2) {
+                        compensator = bufferedReader.readLine();
+                    } System.out.println(bufferedReader.readLine());
+                    break;
+
+                case "q":
+                    fileReader.close();
+                    return;
+            }
+        } while (!charMenu.equals("q"));
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -9,73 +9,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Parser {
-    private int count = 0;
-
+    private int count;
+    private static String filePath = "Emoji.txt";
     public void Start() throws IOException {
-        List<Article> articleList = new ArrayList<>();                                                                  //  Создаем массив articleList
+        List<Article> articleList = new ArrayList<>();
 
-        Document doc = Jsoup.connect("https://unicode-table.com/ru/emoji/").get();                                  //  Получаем HTML код страницы
+        Document doc = Jsoup.connect("https://unicode-table.com/ru/emoji/").get();
 
-        Elements divElements = doc.getElementsByAttributeValue("class", " character-list__link js-char-popup-item");    //  Получаем класс character-list__link js-char-popup-item
-        File filePathName = new File("Emoji.txt");                                                             //  Указываем название файла для хранения емоджи
-        FileWriter nFile = new FileWriter(filePathName);                                                                //  Создаем текстовый файл
+        Elements divElements = doc.getElementsByAttributeValue("class", " character-list__link js-char-popup-item");
+        FileWriter fileWriter = new FileWriter(filePath);
 
-        divElements.forEach(divElement -> {                                                                             //  Проходимся по каждому елементу divElement
-            String uname = divElement.text();                                                                           //  Парсим uname - Код емоджи
-            String utitle = divElement.attr("title");                                                         //  Парсим utitle - Описание
-            String ucode = divElement.attr("data-symbol");                                                    //  Парсим ucode - Емоджи
+        divElements.forEach(divElement -> {
+            String uname = divElement.text();
+            String ucode = divElement.attr("data-symbol");
 
-            articleList.add(new Article(uname, utitle, ucode));                                                         //  Записываем спарсенные данные в массив
-            try {                                                                                                       //
-                nFile.write(String.valueOf(articleList.get(count++) + "\n"));                                       //
-            } catch (IOException e) {                                                                                   //  Выгрузка строк массива в текстовый файл
-                throw new RuntimeException(e);                                                                          //
-            }                                                                                                           //
+            articleList.add(new Article(uname, ucode));
+            try {
+                fileWriter.write(String.valueOf(articleList.get(count++) + "\n"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
 
-        nFile.close();
+        fileWriter.close();
     }
 }
 
 class Article {
     private String uname;
-    private String utitle;
     private String ucode;
 
-    public Article(String uname, String utitle, String ucode) {
+    public Article(String uname, String ucode) {
         this.uname = uname;
-        this.utitle = utitle;
         this.ucode = ucode;
     }
 
-    public String setUname() {
-        return uname;
-    }
+    public String setUname() { return uname; }
 
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
+    public void setUname(String uname) { this.uname = uname; }
 
-    public String getUtitle() {
-        return utitle;
-    }
+    public String getUcode() { return ucode; }
 
-    public void setUtitle(String utitle) {
-        this.utitle = utitle;
-    }
+    public void setUcode(String ucode) { this.ucode = ucode; }
 
-    public String getUcode() {
-        return ucode;
-    }
-
-    public void setUcode(String ucode) {
-        this.ucode = ucode;
-    }
-
-    @Override                                                                                                           //
-    public String toString() {                                                                                          //
-        return  "uname='" + uname + '\'' +                                                                              //  Шаблон заполнения массива
-                ", utitle='" + utitle + '\'' +                                                                          //
-                ", ucode='" + ucode + '\'';                                                                             //
+    @Override
+    public String toString() {
+        return  uname + "\n" + ucode;
     }
 }
